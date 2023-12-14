0b8dddd2fa92f3ea2ab99e45f2ff39e0561fca33
Oscar Davies <oscar.william.davies@outlook.com>
2023-12-14 23:58:33 +0000

    Top Bar

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,6 @@ import ProgressBar from "./components/progressBar";
 import Padd from "./components/padd";
 import {Component} from "react";
 import APIRequest from "./components/request";
-import axios from "axios";
 
 export default class App extends Component{
     constructor(props) {
@@ -31,17 +30,23 @@ export default class App extends Component{
         return (
             <View style={styles.outer}>
                 <View style={styles.container}>
-                    <Text>Open up App.js to start working on your app!</Text>
-                    <Text>Will This Overrun?Will This Overrun?Will This Overrun?Will This Overrun?Will This Overrun?Will This Overrun?Will This Overrun?Will This Overrun?Will This Overrun?</Text>
-                    <Padd>
-                        <Button title={"I Like DIICCK"} onPress={x => this.getStatus()}/>
-                    </Padd>
+                    <View style={styles.topBar}>
+                        <Text>Status Tracker</Text>
+                    </View>
+                    <View style={styles.body}>
+                        <Text>Open up App.js to start working on your app!</Text>
+                        <Text>Will This Overrun?Will This Overrun?Will This Overrun?Will This Overrun?Will This Overrun?Will This Overrun?Will This Overrun?Will This Overrun?Will This Overrun?</Text>
+                        <Padd>
+                            <Button title={"I Like DIICCK"} onPress={x => this.getStatus()}/>
+                        </Padd>
 
-                    <Padd>
-                        <ProgressBar progress={0.2}>%</ProgressBar>
-                    </Padd>
+                        <Padd>
+                            <ProgressBar progress={0.2}>%</ProgressBar>
+                        </Padd>
 
-                    <Text>{this.state.smeatonStatus.UpForMS}</Text>
+                        <Text>{this.state.smeatonStatus.UpForMS}</Text>
+                    </View>
+
                     {/*<StatusBar style="auto"/>*/}
                 </View>
             </View>
@@ -57,22 +62,33 @@ const maxAspect = 16/(16+9);
 const tarAspect = Math.min(windowAspect, maxAspect);
 
 const styles = StyleSheet.create({
+    topBar:{
+        //flex: 1,
+        minWidth: "100%",
+        alignSelf: "flex-start",
+        alignItems: "center"
+    },
     outer:{
         flex: 1,
         backgroundColor: '#565656',
         alignItems: 'center',
         justifyContent: 'center'
     },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    //aspectRatio: tarAspect,
-      maxHeight: windowHeight,
-      maxWidth: windowHeight * tarAspect,
-      padding: 10,
-      overflowY: "scroll",
-
-},
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        //justifyContent: 'center',
+        //aspectRatio: tarAspect,
+        maxHeight: windowHeight,
+        maxWidth: windowHeight * tarAspect,
+        padding: 10,
+        overflowY: "scroll"
+    },
+    body:{
+        flex: 1,
+        alignSelf: "center",
+        justifyContent: "center",
+        alignItems: "center"
+    }
 });
