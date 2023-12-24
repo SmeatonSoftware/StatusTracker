1c4c0da6ce8c91bda77f7d9ffadd35f72d673039
Oscar <>
2023-12-24 21:48:55 +0000

    FIx Laptop Build

--- mobile/build.sh
+++ mobile/build.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-export ANDROID_HOME=$HOME/android
+#https://stackoverflow.com/questions/49765669/failed-to-find-android-home-environment-variable-in-windows-10-linux-subsystem
+
+#export ANDROID_HOME=$HOME/android
+export ANDROID_HOME="/mnt/c/Users/oscar/AppData/Local/Android/sdk"
+
 export ANDROID_SDK_ROOT=${ANDROID_HOME}
 export PATH=${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${PATH}
 
